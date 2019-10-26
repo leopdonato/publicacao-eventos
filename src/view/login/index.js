85b192c0a897aab053adc9d853ba7b5e9daf1e0b
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-10-26 20:18:42 -0300

    Desenvolvendo Layout da tela de login

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import './login.css';
 
 function Login() {
-    return(
-        <h1>Tela de Login</h1>
+    return (
+        <div className="login-content d-flex align-items-center">
+            <form className="form-signin mx-auto">
+                <div className="text-center mb-4">
+                    <img className="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
+                    <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>
+
+                </div>
+
+                <input type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
+                <input type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
+
+                <button className="btn btn-lg btn-block btn-login" type="submit">Sign in</button>
+
+                <div className="msg-login text-white text-center my-5 text-center">
+                    <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
+                    <br />
+                    <span><strong>Ops!</strong> Verifique se a senha ou usuário estão corretos! &#128546;</span>
+                </div>
+
+                <div className="opcoes-login mt-5">
+                    <a href="#" className="mx-2">Recuperar Senha</a>
+                    <span className="text-white">&#9733;</span>
+                    <a href="#" className="mx-2">Quero Cadastrar</a>
+                </div>
+            </form>
+        </div>
     );
 }
 
