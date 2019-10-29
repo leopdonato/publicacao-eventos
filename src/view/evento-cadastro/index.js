40d8e835e7880c229f3a82b2f87b69fac7ad609a
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-10-29 12:16:33 -0300

    Cadastro de evento com firestore do firebase

--- src/view/evento-cadastro/index.js
+++ src/view/evento-cadastro/index.js
@@ -8,6 +8,43 @@ import Navbar from '../../components/navbar';
 function EventoCadastro() {
 
     const [msgTipo, setMsgTipo] = useState();
+    const [titulo, setTitulo] = useState();
+    const [tipo, setTipo] = useState();
+    const [detalhes, setDetalhes] = useState();
+    const [data, setData] = useState();
+    const [hora, setHora] = useState();
+    const [foto, setFoto] = useState();
+    const usuarioEmail = (useSelector(state => state.usuarioEmail));
+    const [carregando, setCarregando] = useState();
+
+    const storage = firebase.storage();
+    const db = firebase.firestore();
+
+    function cadastrar() {
+        setMsgTipo(null);
+        setCarregando(1);
+
+        storage.ref(`imagens/${foto.name}`).put(foto).then(() => {
+            db.collection('eventos').add({
+                titulo: titulo,
+                tipo: tipo,
+                detalhes: detalhes,
+                data: data,
+                hora: hora,
+                usuario: usuarioEmail,
+                visualizacoes: 0,
+                foto: foto.name,
+                publico: 1,
+                criacao: new Date()
+            }).then(() => {
+                setMsgTipo('sucesso');
+                setCarregando(0);
+            }).catch(err => {
+                setMsgTipo('erro');
+                setCarregando(0);
+            });
+        });
+    }
 
     return (
         <>
@@ -19,11 +56,11 @@ function EventoCadastro() {
                 <form>
                     <div className="form-group">
                         <label>Título:</label>
-                        <input type="text" className="form-control" />
+                        <input onChange={(e) => setTitulo(e.target.value)} type="text" className="form-control" />
                     </div>
                     <div className="form-group">
                         <label>Tipo do evento:</label>
-                        <select className="form-control">
+                        <select onChange={(e) => setTipo(e.target.value)} className="form-control">
                             <option disabled selected value>-- Selecione um tipo --</option>
                             <option>Festa</option>
                             <option>Teatro</option>
@@ -33,24 +70,33 @@ function EventoCadastro() {
                     </div>
                     <div className="form-group">
                         <label>Descrição do evento:</label>
-                        <textarea className="form-control" rows="3" />
+                        <textarea onChange={(e) => setDetalhes(e.target.value)} className="form-control" rows="3" />
                     </div>
                     <div className="form-group row">
                         <div className="col-6">
                             <label>Data:</label>
-                            <input type="date" className="form-control" />
+                            <input onChange={(e) => setData(e.target.value)} type="date" className="form-control" />
                         </div>
                         <div className="col-6">
                             <label>Hora:</label>
-                            <input type="time" className="form-control" />
+                            <input onChange={(e) => setHora(e.target.value)} type="time" className="form-control" />
                         </div>
                     </div>
                     <div className="form-group">
                         <label>Upload da foto:</label>
-                        <input type="file" className="form-control" />
+                        <input onChange={(e) => setFoto(e.target.files[0])} type="file" className="form-control" />
                     </div>
 
-                    <button type="button" className="btn btn-lg btn-block mt-3 mb-4 btn-publicar">Publicar Evento</button>
+                    <div className="row">
+                        {
+                            carregando > 0 ?
+                                <div class="spinner-border text-danger mx-auto" role="status">
+                                    <span class="sr-only">Carregando...</span>
+                                </div>
+                                :
+                                <button onClick={cadastrar} type="button" className="btn btn-lg btn-block mt-3 mb-4 btn-publicar">Publicar Evento</button>
+                        }
+                    </div>
 
                 </form>
 
