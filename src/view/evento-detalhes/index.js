79c087c89a5157f7b7784b1945621d3e87b64712
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-11-05 11:38:19 -0300

    Refinando carregamento da página detalhes

--- src/view/evento-detalhes/index.js
+++ src/view/evento-detalhes/index.js
@@ -10,11 +10,15 @@ function EventoDetalhes(props) {
     const [evento, setEvento] = useState({});
     const [urlImg, setUrlImg] = useState({});
     const usuarioLogado = useSelector(state => state.usuarioEmail);
+    const [carregando, setCarregando] = useState(1);
 
     useEffect(() => {
         firebase.firestore().collection('eventos').doc(props.match.params.id).get().then(res => {
             setEvento(res.data());
-            firebase.storage().ref(`imagens/${evento.foto}`).getDownloadURL().then(url => setUrlImg(url));
+            firebase.storage().ref(`imagens/${evento.foto}`).getDownloadURL().then(url => {
+                setUrlImg(url)
+                setCarregando(0);
+            });
         });
     });
 
@@ -22,47 +26,53 @@ function EventoDetalhes(props) {
         <>
             <Navbar />
             <div className="container-fluid">
-                <div className="row">
-                    <img src={urlImg} className="img-banner" alt="Banner" />
-                    <div className="col-12 text-right mt-1 visualizacoes">
-                        <i className="fas fa-eye"></i><span className="ml-2">{evento.visualizacoes}</span>
-                    </div>
-                    <h3 className="mx-auto mt-5 titulo"><strong>{evento.titulo}</strong></h3>
-                </div>
-                <div className="row mt-5 d-flex justify-content-around">
-                    <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                        <i className="fas fa-ticket-alt fa-2x"></i>
-                        <h5><strong>Tipo</strong></h5>
-                        <span className="mt-3">{evento.tipo}</span>
-                    </div>
-
-                    <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                        <i className="fas fa-calendar-alt fa-2x"></i>
-                        <h5><strong>Data</strong></h5>
-                        <span className="mt-3">{evento.data}</span>
-                    </div>
-
-                    <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                        <i className="fas fa-clock fa-2x"></i>
-                        <h5><strong>Hora</strong></h5>
-                        <span className="mt-3">{evento.hora}</span>
-                    </div>
-                </div>
-
-                <div className="row box-detalhes mt-5">
-                    <div className="col-12 text-center">
-                        <h5><strong>Detalhes evento</strong></h5>
-                    </div>
-                    <div className="col-12 text-center">
-                        <p>{evento.detalhes}</p>
-                    </div>
-                </div>
                 {
-                    usuarioLogado === evento.usuario ?
-                        <Link to="" className="btn-editar"><i className="fas fa-pen-square fa-3x"></i></Link>
-                        : ''
-                }
+                    carregando ? <div className="row mt-5"> <div class="spinner-border text-danger mx-auto" role="status"><span class="sr-only">Carregando...</span></div></div>
 
+                        :
+                        <div>
+                            <div className="row">
+                                <img src={urlImg} className="img-banner" alt="Banner" />
+                                <div className="col-12 text-right mt-1 visualizacoes">
+                                    <i className="fas fa-eye"></i><span className="ml-2">{evento.visualizacoes}</span>
+                                </div>
+                                <h3 className="mx-auto mt-5 titulo"><strong>{evento.titulo}</strong></h3>
+                            </div>
+                            <div className="row mt-5 d-flex justify-content-around">
+                                <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                                    <i className="fas fa-ticket-alt fa-2x"></i>
+                                    <h5><strong>Tipo</strong></h5>
+                                    <span className="mt-3">{evento.tipo}</span>
+                                </div>
+
+                                <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                                    <i className="fas fa-calendar-alt fa-2x"></i>
+                                    <h5><strong>Data</strong></h5>
+                                    <span className="mt-3">{evento.data}</span>
+                                </div>
+
+                                <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                                    <i className="fas fa-clock fa-2x"></i>
+                                    <h5><strong>Hora</strong></h5>
+                                    <span className="mt-3">{evento.hora}</span>
+                                </div>
+                            </div>
+
+                            <div className="row box-detalhes mt-5">
+                                <div className="col-12 text-center">
+                                    <h5><strong>Detalhes evento</strong></h5>
+                                </div>
+                                <div className="col-12 text-center">
+                                    <p>{evento.detalhes}</p>
+                                </div>
+                            </div>
+                            {
+                                usuarioLogado === evento.usuario ?
+                                    <Link to="" className="btn-editar"><i className="fas fa-pen-square fa-3x"></i></Link>
+                                    : ''
+                            }
+                        </div>
+                }
             </div>
         </>
     )
