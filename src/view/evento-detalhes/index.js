ed570961957ca5b727ab705967257c5f49b872ca
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-11-05 00:52:03 -0300

    Carregando detalhes do evento do banco de dados na página

--- src/view/evento-detalhes/index.js
+++ src/view/evento-detalhes/index.js
@@ -1,47 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './evento-detalhes.css';
 import firebase from '../../config/firebase'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from '../../components/navbar';
 
-function eventoDetalhes() {
+function EventoDetalhes(props) {
+
+    const [evento, setEvento] = useState({});
+    const [urlImg, setUrlImg] = useState({});
+    const usuarioLogado = useSelector(state => state.usuarioEmail);
+
+    useEffect(() => {
+        firebase.firestore().collection('eventos').doc(props.match.params.id).get().then(res => {
+            setEvento(res.data());
+            firebase.storage().ref(`imagens/${evento.foto}`).getDownloadURL().then(url => setUrlImg(url));
+        });
+    });
+
     return (
         <>
             <Navbar />
             <div className="container-fluid">
                 <div className="row">
-                    <img src="https://via.placeholder.com/150x100" className="img-banner" alt="Banner" />
+                    <img src={urlImg} className="img-banner" alt="Banner" />
+                    <div className="col-12 text-right mt-1 visualizacoes">
+                        <i className="fas fa-eye"></i><span className="ml-2">{evento.visualizacoes}</span>
+                    </div>
+                    <h3 className="mx-auto mt-5 titulo"><strong>{evento.titulo}</strong></h3>
                 </div>
                 <div className="row mt-5 d-flex justify-content-around">
                     <div className="col-md-3 col-sm-12 box-info p-3 my-2">
                         <i className="fas fa-ticket-alt fa-2x"></i>
                         <h5><strong>Tipo</strong></h5>
-                        <span className="mt-3">Festa</span>
+                        <span className="mt-3">{evento.tipo}</span>
                     </div>
 
                     <div className="col-md-3 col-sm-12 box-info p-3 my-2">
                         <i className="fas fa-calendar-alt fa-2x"></i>
                         <h5><strong>Data</strong></h5>
-                        <span className="mt-3">10/10/2019</span>
+                        <span className="mt-3">{evento.data}</span>
                     </div>
 
                     <div className="col-md-3 col-sm-12 box-info p-3 my-2">
                         <i className="fas fa-clock fa-2x"></i>
                         <h5><strong>Hora</strong></h5>
-                        <span className="mt-3">19:00 Horas</span>
+                        <span className="mt-3">{evento.hora}</span>
                     </div>
                 </div>
 
                 <div className="row box-detalhes mt-5">
-                    <h5 className="mx-auto"><strong>Detalhes evento</strong></h5>
-                    <p className="text-justify p-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop</p>
+                    <div className="col-12 text-center">
+                        <h5><strong>Detalhes evento</strong></h5>
+                    </div>
+                    <div className="col-12 text-center">
+                        <p>{evento.detalhes}</p>
+                    </div>
                 </div>
+                {
+                    usuarioLogado === evento.usuario ?
+                        <Link to="" className="btn-editar"><i className="fas fa-pen-square fa-3x"></i></Link>
+                        : ''
+                }
 
-                <Link to="" className="btn-editar"><i className="fas fa-pen-square fa-3x"></i></Link>
             </div>
         </>
     )
 }
 
-export default eventoDetalhes;
+export default EventoDetalhes;
